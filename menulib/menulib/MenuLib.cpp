1379a53d45fa2683675240f4c90f2b5a8d48ebf6
bibindon0814 <bibindon0814@gmail.com>
2024-11-18 10:27:10 +0900

    .

--- menulib/menulib/MenuLib.cpp
+++ menulib/menulib/MenuLib.cpp
@@ -3,7 +3,7 @@
 
 using namespace NSMenulib;
 
-std::vector<std::string> split(const std::string& s, char delim)
+static std::vector<std::string> split(const std::string& s, char delim)
 {
     std::vector<std::string> result;
     std::stringstream ss(s);
